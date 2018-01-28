34cd45ec62c42ec46ec26541c0e61d1ce39ecd07
Daniel Vysotskiy <dordarus@gmail.com>
2018-01-28 13:41:31 +0200

    Adding abillity to adding new author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,5 +7,23 @@ class AuthorsController < ApplicationController
         @author = Author.find(params[:id])
     end
 
+    def new
+       @author = Author.new
+    end   
+
+    def create
+        @author = Author.new(author_params)
+        if @author.save
+            redirect_to '/authors'
+        else
+            render 'new'
+        end
+    end   
+
+    private 
+    def author_params
+        params.require(:author).permit(:first_name, :last_name, :bio)
+    end
+   
     #TODO: Add editig and deletion
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/authors'=>'authors#index'
+  get '/authors/new'=> 'authors#new', as: :new_author
   get '/authors/:id' => 'authors#show', as: :author
-  get '/books'=>'books#index'
+  post '/authors'=>'authors#create'
   get '/books/:id'=> 'books#show', as: :book
+
 end
