62a984bb3f62da1594e92eb107ce34c74f182f59
Daniel Vysotskiy <dordarus@gmail.com>
2018-01-29 19:13:19 +0200

    Finish without styles

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,22 +2,18 @@ class BooksController < ApplicationController
 
     before_action :find_book, only: [:show, :edit, :update, :destroy]
 
-    #done
     def index
         @books = Book.all        
     end
 
-    #done
     def show        
     end
 
-    #done
     def new
         @book = Book.new
         @author = Author.find(params[:a_id])
     end
 
-    #done
     def create
         @book = Book.new(book_params)
         if @book.save
@@ -27,11 +23,9 @@ class BooksController < ApplicationController
         end
     end
 
-    #done
     def edit        
     end
 
-    #done
     def update
         if @book.update(book_params)
             redirect_to @book
@@ -40,17 +34,20 @@ class BooksController < ApplicationController
         end
     end 
 
-    #TODO
     def destroy
+        @author = @book.author
+        if @book.destroy
+            redirect_to author_path(@author)
+        else
+            redirect_to root
+        end
     end
 
-    #done
     private
     def book_params
         params.require(:book).permit(:title, :genre, :year, :plot, :author_id)
     end
 
-    #done
     def find_book
         @book = Book.find(params[:id])
     end
