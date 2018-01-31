23ab49dd483a0d3d2cc5468e4f3777fd88289f9a
Daniel Vysotskiy <dordarus@gmail.com>
2018-01-31 13:01:16 +0200

    Change test_unit to rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root 'authors#index'
   #authors routes
-  get '/authors'=>'authors#index'
+  get '/authors'=>'authors#index', as: :authors_index
   get '/authors/new'=> 'authors#new', as: :new_author
   post '/authors'=>'authors#create'
   get '/authors/:id' => 'authors#show', as: :author
   get '/authors/:id/edit'=>'authors#edit', as: :edit_author
   patch '/authors/:id' => 'authors#update'
   #books routes
-  get '/books' => 'books#index'
+  get '/books' => 'books#index', as: :books_index
   get '/books/new/:a_id'=> 'books#new', as: :new_book
   post '/books'=>'books#create'
   get '/books/:id'=> 'books#show', as: :book
